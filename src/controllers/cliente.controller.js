4a7f58c48f7234a1352619d3f52a68c0eff6c4ec
dgamay <54776007+dgamay@users.noreply.github.com>
2026-03-01 19:32:26 -0500

    Update cliente.controller.js   crud  completo
    se agrego la actualizacion y eliminacion del cliente

--- src/controllers/cliente.controller.js
+++ src/controllers/cliente.controller.js
@@ -15,3 +15,39 @@ exports.getByCobrador = async (req, res) => {
 
     res.json(clientes);
 };
+
+//Actualizar cliente
+exports.update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const clienteActualizado = await Cliente.findByIdAndUpdate(
+      id, 
+      req.body, 
+      { new: true } // Para que devuelva el documento actualizado
+    );
+    
+    if (!clienteActualizado) {
+      return res.status(404).json({ error: 'Cliente no encontrado' });
+    }
+    
+    res.json(clienteActualizado);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// ELIMINAR CLIENTE
+exports.delete = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const clienteEliminado = await Cliente.findByIdAndDelete(id);
+    
+    if (!clienteEliminado) {
+      return res.status(404).json({ error: 'Cliente no encontrado' });
+    }
+    
+    res.json({ message: 'Cliente eliminado correctamente' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
